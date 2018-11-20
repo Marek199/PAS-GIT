ce062031f8c7f8898a5a37e4cd056a229db9bf2d
Marek199 <37554060+Marek199@users.noreply.github.com>
2018-11-20 08:56:31 +0100

    Create rejestracja.php

---
+++ rejestracja.php
@@ -0,0 +1,23 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<HEAD>
+<title>PLUTA</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+Formularz rejestracji klienta
+</HEAD>
+<BODY>
+<form method="POST" action="sprawdz.php">
+<br>
+Login:<input type="text" name="uzytkownik" maxlength="20" size="10"><br>
+Haslo:<input type="text" name="haslo" maxlength="20" size="10"><br>
+Powtórz Haslo:<input type="text" name="haslo_again" maxlength="20" size="10"><br>
+<input type="submit" value="Zarejestruj"/>
+</form>
+
+<br>
+
+
+
+
+
+</BODY>
+</HTML>
