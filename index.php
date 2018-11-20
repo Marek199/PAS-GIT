5842f4fce923737bfa8c0666839d80cc3da4619c
Marek199 <37554060+Marek199@users.noreply.github.com>
2018-11-20 17:22:33 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<Title>Pluta</Title>
+<a href="logowanie.php">Logowanie</a></br>
+<a href="rejestracja.php">Rejestracja</a></br>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<BODY>
+</form>
+</BODY>
+</HTML>
