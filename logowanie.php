a6c5b5868c051a9392fbf6d57785cd1e8501823b
Marek199 <37554060+Marek199@users.noreply.github.com>
2018-11-20 08:52:20 +0100

    Create logowanie.php

---
+++ logowanie.php
@@ -0,0 +1,20 @@
+
+<?php
+unset($_COOKIE['user']);
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<title>PLUTA</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<BODY>
+Logowanie do panelu klienta
+<form method="post" action="weryfikuj.php">
+Login:<input type="text" name="user" maxlength="20" size="20"><br>
+Hasło:<input type="password" name="pass" maxlength="20" size="20"><br>
+<input type="submit" value="Send"/>
+</form>
+<a href="rejestracja.php">Rejestracja</a></br>
+<a href='../index.php'>Wstecz</a></br>
+</BODY>
+</HTML>
