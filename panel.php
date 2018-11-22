25bb75d1d95e11e5b9d37694463b04ea1d52770c
Marek199 <37554060+Marek199@users.noreply.github.com>
2018-11-22 17:23:44 +0100

    Update panel.php

--- panel.php
+++ panel.php
@@ -10,47 +10,125 @@ if ($_COOKIE['user']=='')
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <BODY>
-PANEL KLIENTA
+PANEL Uzytkownika
 <a href="logowanie.php">Wyloguj</a></br>
+
+<form method="POST" action="panel.php">
+<br>
+Podaj nazwe folderu:
+<input type="text" name="nazwa_folderu" maxlength="20" size="10"><br>
+<input type="submit" value="Utwórz"/>
+</form>
+
+
+
+
+
 
 
 <?php
-
+$nazwa= $_POST['nazwa_folderu'];
 $przegladarka=$dane['name'];
 $system=$dane['platform'];
 $ip_odwiedzajacego = $_SERVER["REMOTE_ADDR"];
 $czas_obecny = date("y:m:d:H:i:s", time());
 $user=$_COOKIE['user'];
 
-
-echo 'Zalogowany jako: '.$_COOKIE['user'];
-echo "<br></br>";
-echo "IP:$ip_odwiedzajacego";
-		
 		$dbhost='xxx'; 
 		$dbuser="xxx"; 
 		$dbpassword="xxx";
 		$dbname="xxx";
-		$port = 'xzxx';
-		
+		$port = '80';
 		$polaczenie = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname);
-		if(!$polaczenie) { echo"Błąd: ". mysqli_connect_errno()." ".mysqli_connect_error(); }
-		mysqli_query($polaczenie, "SET NAMES 'utf8'");
-		mysqli_query($polaczenie, "INSERT INTO log_klient (nazwa_klienta,IP,przegladarka,datagodz,system) values ('$user','$ip_odwiedzajacego','$przegladarka','$czas_obecny','$system')"); 
-		//mysql_close();
-	//	$result = mysqli_query($polaczenie, "SELECT idKlient FROM Klient WHERE user='$user'");
-	//	if ($wiersz = mysqli_fetch_array ($result)) 	
-	//		{ 	
-	//			
-	//			echo $id=$wiersz[0];
-	//			if (!empty($_POST['pytanie']))
-	//			{
-	//				mysqli_query($polaczenie, "INSERT INTO pytania (id_klienta,user,kategoria,pytanie) values ('$id','$user','$kategoria','$zapytanie')");
-	//			}
+		
+		 if ( isset ( $_POST['nazwa_folderu'] ) )
+		{
+			$katalog = $nazwa;
+			if(!file_exists("./$user/$katalog"))
+
+			{
+				mkdir("./$user/$katalog", 0777);
+				echo "<br>Utworzono folder o nazwie $nazwa</br>";
+				mysqli_query($polaczenie, "INSERT INTO foldery (nazwa,user) values ('$katalog','$user')"); 
+				
+			}else
+			{
+				echo "<br>Folder o podanej nazwie juz istnieje</br>";
+			}				
 			
-	//		}
+			
+		}
+		
+		
+		
+		
+
+if(!$polaczenie) { echo"Błąd: ". mysqli_connect_errno()." ".mysqli_connect_error(); }
+mysqli_query($polaczenie, "SET NAMES 'utf8'");
+$result = mysqli_query($polaczenie, "SELECT nazwa FROM foldery where user='$user'");
+
+echo"Wybierz katalog";
+echo"<form action='odbierz.php' method='POST'ENCTYPE='multipart/form-data'>";
+
+echo"<select name='problemo'>";
+
+echo"<option  selected='selected' value=''>Obecny</option>";
+echo"<br></br>";
+while ($wiersz = mysqli_fetch_array ($result)) 	
+			{ 	
+				echo $id=$wiersz[0];
+			echo"<option  value='$id'>$id</option>";
+			}
+
+
+//echo"<br>";
+//echo"</select>";
+//echo"<br>";
+
+echo"<input type='file' name='plik'/>";
+echo"<input type='submit' value='Wyślij plik'/>";
+echo"</form>";
+
+echo"<form method='POST' action='pobieranie.php'>";
+echo"<select name='pobieranie'>";
+echo"<option  selected='selected' value=''>Macierzysty</option>";
+$result = mysqli_query($polaczenie, "SELECT nazwa FROM foldery where user='$user'");
+while ($wiersz = mysqli_fetch_array ($result)) 	
+			{ 	
+				echo $id=$wiersz[0];
+			echo"<option  value='$id'>$id</option>";
+			}
+
+echo"<input type='submit' value='Przejdz do katalogu'/>";
+echo"</form>";
+
+
+
+
+
+
+
+
+?>
+<?php
+echo"<br></br>";
+echo 'Zalogowany jako: '.$_COOKIE['user'];
+echo"<br></br>";
+echo "<br></br>";
+echo "IP:$ip_odwiedzajacego";
+		
+		
+		
+		
+		
+		
+		
+	//	mysqli_query($polaczenie, "INSERT INTO log_klient (nazwa_klienta,IP,przegladarka,datagodz,system) values ('$user','$ip_odwiedzajacego','$przegladarka','$czas_obecny','$system')"); 
+		//mysql_close();
+	//	
+		
 	
-		 
+		
 		
 		
 		
